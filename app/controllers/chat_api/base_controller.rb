f199f9914023f277caf1cba34199a45ecd5ff3a1
Artur Trofimov <arturtr@gmail.com>
2016-03-07 03:41:10 +1000

    Auth update

--- app/controllers/chat_api/base_controller.rb
+++ app/controllers/chat_api/base_controller.rb
@@ -1,24 +1,24 @@
 module ChatApi
   class BaseController < ApplicationController
-    before_action :authorize
+    before_filter :authorize
 
     private
 
     def authorize
-      if params[:token].present?
-        user_id, time_int, salt = Base64.decode64(params[:token]).split(':')
-        if salt == 'guest'
-          @chat_user = ChatUser.new(user_id)
-        else
-          @user = User.find(user_id)
-          if @user.salt == salt
+      if session[:user_id]
+        # user is authenticated
+        @chat_user = ChatUser.new(session[:user_id])
+      else
+        if params[:token].present?
+          user_id, time_int, salt = Base64.decode64(params[:token]).split(':')
+          if salt == 'guest'
             @chat_user = ChatUser.new(user_id)
           else
             render json: {error: 'wrong token'}, status: :unauthorized
           end
+        else
+          render json: {error: 'empty token'}, status: :unauthorized
         end
-      else
-        render json: {error: 'empty token'}, status: :unauthorized
       end
     end
   end
