a8839dc02321e4f9aa490171cb844107adec967e
Vladimir Luss <stainful@gmail.com>
2016-03-08 19:30:57 +0300

    added fetch polyfill

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -21,6 +21,9 @@ module.exports = {
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin(),
+        new webpack.ProvidePlugin({
+                'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        }),
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
