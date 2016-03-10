ec60d9a42491af5bbc1fdd6c6073711729a818a7
Vladimir Luss <stainful@gmail.com>
2016-03-10 06:50:14 -0500

    join chat button

--- lib/redmine_chat/hook_listener.rb
+++ lib/redmine_chat/hook_listener.rb
@@ -1,4 +1,5 @@
 class ChatHookListener < Redmine::Hook::ViewListener
+  render_on :view_issues_show_details_bottom, :partial => 'shared/join_chat_button'
   render_on :view_layouts_base_body_bottom, partial: 'shared/chat'
   render_on :view_layouts_base_html_head, partial: 'shared/chat_css'
 end
