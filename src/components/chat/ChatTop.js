dacb0956e1bc9d3ec4ef5c3588051390de57fefd
Vladimir Luss <stainful@gmail.com>
2016-03-16 22:39:17 +0300

    minimize button

--- src/components/chat/ChatTop.js
+++ src/components/chat/ChatTop.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Icon from 'react-fa';
 
 import styles from 'styles/chat.module.css';
 
 export default class ChatTop extends React.Component {
     render() {
         return (
-            <div className={styles.chat__topbar}></div>
+            <div className={styles.chat__topbar}>
+                <Icon onClick={this.props.onMinimize} className={styles.chat__topbar__close} name="times"/>
+            </div>
         )
     }
 }
--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -5,17 +5,14 @@ import ChatTop from 'components/chat/ChatTop';
 import MessageList from 'components/chat/MessageList';
 import MessageComposer from 'components/chat/MessageComposer';
 
-import * as actions from '../actions/actions';
+import * as actions from 'actions/actions';
+import { minimizeChat } from 'actions/UI';
+
 import styles from 'styles/chat.module.css';
 
 class ChatContainer extends React.Component {
     componentWillMount() {
-        const { dispatch, user } = this.props;
-        dispatch(actions.fetchMessages(1));
-    }
-    _sendMessage(message) {
-        const {dispatch} = this.props;
-        dispatch(actions.sendMessage(this.props.currentChannel, message))
+        this.props.fetchMessages();
     }
     _deleteMessage(msgId) {
         const {dispatch} = this.props;
@@ -23,27 +20,53 @@ class ChatContainer extends React.Component {
     }
     render() {
         if (this.props.currentChannel === null) {
-            return <div className={styles.container}>выберите чат</div>
+            return (
+                <div className={styles.chatcontainer}>
+                    <ChatTop onMinimize={this.props.minimizeChat}/>
+                    выберите чат
+                </div>
+            )
         }
         return (
             <div className={styles.chatcontainer}>
-                <ChatTop/>
+                <ChatTop onMinimize={this.props.minimizeChat}/>
                 <div className={styles.channel_title}>{this.props.channelTitle}</div>
                 <MessageList 
                     channelTitle={this.props.channelTitle}
                     messages={this.props.messages} 
                     onDeleteMessage={::this._deleteMessage}
                 />
-                <MessageComposer sendMessage={::this._sendMessage}/>
+                <MessageComposer sendMessage={this.props.sendMessage}/>
             </div>
         )
     }
 }
 
-export default connect((state) => {
+const mapStateToProps = state => {
     return {
         currentChannel: state.account.currentChannel,
         channelTitle: state.account.currentChannel ? state.chats.channels[state.account.currentChannel].title : null,
         messages: state.chats.messages[state.account.currentChannel] || null
     }
-})(ChatContainer);
+}
+
+const mapDispatchToProps = {
+    ...actions,
+    minimizeChat
+}
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+    return Object.assign({}, stateProps, dispatchProps, {
+        fetchMessages: () => {
+            if (stateProps.currentChannel !== null)
+                return dispatchProps.fetchMessages(stateProps.currentChannel);
+        },
+        sendMessage: (message) => {
+            if (stateProps.currentChannel !== null ) {
+                return dispatchProps.sendMessage(stateProps.currentChannel, message);
+            }
+        }
+    });
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(ChatContainer);
