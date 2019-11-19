de046f9d5289f7d343d5f92a39aac33066ccae0f
Igor Olemskoi <igor@southbridge.ru>
2019-11-19 23:05:14 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -7,11 +7,11 @@ require_dependency 'chat/patches/application_helper_patch'
 
 Redmine::Plugin.register :redmine_chat do
   name 'Redmine Chat plugin'
-  url 'https://github.com/centosadmin/redmine_chat'
+  url 'https://github.com/southbridgeio/redmine_chat'
   description 'This is a plugin for implement chat messaging in Redmine'
   version '1.3.0'
-  author 'Centos-admin.ru'
-  author_url 'http://centos-admin.ru'
+  author 'Southbridge'
+  author_url 'https://southbridge.io'
 
   project_module :chat do
     permission :chatting, :chat_messages => :index
